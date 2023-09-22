87113d9dce3fc9cfa12f0e1ff62b3c34ee71408b
garcelleraseantieralaveta <117034943+garcelleraseantieralaveta@users.noreply.github.com>
2023-09-22 10:32:27 +0200

    Update app_test_runner.py

--- app_test_runner.py
+++ app_test_runner.py
@@ -8,3 +8,6 @@ from optparse import OptionParser
 from django.conf import settings
 from django.core.management import call_command
 
+def main():
+    """
+    The entry point for the script. This script is fairly basic. Here is a
