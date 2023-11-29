8c9ca65095b03b7c58e0fe55513b2a12dc3e85b5
garcelleraseantieralaveta <117034943+garcelleraseantieralaveta@users.noreply.github.com>
2023-11-29 11:22:46 +0100

    Update app_test_runner.py

--- app_test_runner.py
+++ app_test_runner.py
@@ -11,3 +11,6 @@ from django.core.management import call_command
 def main():
     """
     The entry point for the script. This script is fairly basic. Here is a
+ quick example of how to use it::
+    
+        app_test_runner.py [path-to-app]
